353a611f2d0d072ffceb39e76622503e4f0c6d72
Molina-Q <quentinmolina1@gmail.com>
2025-02-07 11:37:18 +0100

    feat: refactor SingleGoal component and enhance priority display with color coding

--- app/(client)/collections/[collectionId]/_components/singleGoal.tsx
+++ app/(client)/collections/[collectionId]/_components/singleGoal.tsx
@@ -1,12 +1,22 @@
 import { GoalType } from "@/types/types";
 
-export function singleGoal(goal: GoalType) {
+export function SingleGoal(goal: GoalType) {
+    const getColorPriority = (priority: number | string) => {
+        return {
+            1: "bg-red-500 border-red-500",
+            2: "bg-orange-500 border-orange-500",
+            3: "bg-yellow-500 border-yellow-500",
+            4: "bg-green-300 border-green-300",
+            5: "bg-green-500 border-green-500",
+        }[priority] || "bg-gray-500 border-gray-500";
+    }
+
 
     return (
-        <div className="bg-gray-900 rounded-xl py-2 px-4">
-            <div>
-                <div>
-                    <input type="radio" checked={goal.isAccomplished} value="done" id={goal.label} />
+        <div className="bg-gray-900 rounded-xl py-2 px-4 my-4 w-full">
+            <div className="flex flex-row justify-between items-center">
+                <div className="flex gap-2 items-center justify-start">
+                    <input type="checkbox" defaultChecked={goal.isAccomplished} value="done" id={goal.label} />
                     <label htmlFor={goal.label}>{goal.label}</label>
                 </div>
 
@@ -14,9 +24,16 @@ export function singleGoal(goal: GoalType) {
             </div>
             {/* divider */}
             <div>
-                <div className="flex flex-row justify-between gap-4">
-                    <h2>{goal.category.label}</h2>
-                    <p>{goal.priority}</p>
+                <div className="flex flex-row justify-between items-center my-4">
+                    <h2 className="text-blue-400">{goal.category.label}</h2>
+                    <p className={`
+                        border-2 rounded-full 
+                        size-7 ${getColorPriority(goal.priority || "0")}
+                        flex justify-center items-center 
+                        text-black`}
+                    >
+                        {goal.priority}
+                    </p>
                 </div>
 
                 <p>{goal.description}</p>

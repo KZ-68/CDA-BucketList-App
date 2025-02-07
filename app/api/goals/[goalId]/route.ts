1bf5f036a3ef21f70fd7312028d0a7b3e2b9aee8
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-02-07 09:08:36 +0100

    feat: add edit method for goal id

--- app/api/goals/[goalId]/route.ts
+++ app/api/goals/[goalId]/route.ts
@@ -6,6 +6,72 @@ type Props = {
     params: Promise<{ goalId: string }>
 }
 
+const editGoalSchema = z.object({
+    label: z.string().nonempty({ message: "Label is required" }),
+    description: z.string().optional(),
+    isAccomplished: z.boolean(),
+    priority: z.number({ message: "Priority must be completed" })
+        .int({ message: "Priority must be an int" })
+        .nonnegative({ message: "Priority must be positive" }),
+    collectionId: z.string().nonempty({ message: "a Collection is required" }),
+    categoryId: z.string().nonempty({ message: "a Category is required" }),
+});
+
+export async function POST(request: NextRequest, { params }: Props) {
+    try {
+        const { goalId } = await params;
+
+        const body = await request.json();
+        const { userId, label, description, isAccomplished, priority, collectionId, categoryId } = body;
+
+        if (!userId || !categoryId) {
+            return NextResponse.json(
+                { error: "User ID and Category ID are required" },
+                { status: 400 }
+            );
+        }
+
+        const goalData = { label, description, isAccomplished, priority, collectionId, categoryId };
+
+        editGoalSchema.parse(goalData);
+
+        const newGoal = await db.goal.update({
+            where: {
+                id: goalId,
+            },
+            data: {
+                label: goalData.label,
+                description: goalData.description,
+                isAccomplished: goalData.isAccomplished,
+                priority: goalData.priority,
+                collectionId: goalData.collectionId,
+                categoryId: goalData.categoryId
+            },
+        });
+
+        return NextResponse.json({
+            success: true,
+            message: "Goal Edited !",
+            data: newGoal,
+        });
+    } catch (error) {
+        console.log("[EDIT GOAL]", error);
+        if (error instanceof z.ZodError) {
+            return NextResponse.json(
+                { error: error.errors[0].message },
+                { status: 400 }
+            );
+        }
+
+        return NextResponse.json({
+            data: null,
+            success: false,
+            message: `edit goal: Internal Error:  ${error || "nothing"}`
+        }, { status: 500 }
+        )
+    }
+}
+
 export async function DELETE(request: NextRequest, { params }: Props) {
     try {
         const { goalId } = await params;

6c8a68d72ec1141240ab3b2cf8d95268e5d0c04f
Molina-Q <quentinmolina1@gmail.com>
2025-02-07 15:42:29 +0100

    fix: adjust padding in Check component for improved layout

--- app/(client)/collections/[collectionId]/_components/Check.tsx
+++ app/(client)/collections/[collectionId]/_components/Check.tsx
@@ -8,7 +8,7 @@ interface CheckProps {
 
 export function Check({state, label}: CheckProps) {
     return (
-        <div className="flex justify-start items-center py-2 pr-4">
+        <div className="flex justify-start items-center py-2 pr-2">
             <input type="checkbox" defaultChecked={state} className='hidden interactive-check-done' id={label}/>
             <label htmlFor={label} defaultChecked={state} className="cursor-pointer">
                 <div className='bg-[rgba(123,136,157,0.33)] rounded-sm flex justify-center items-center size-8'>

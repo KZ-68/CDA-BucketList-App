537b765a494f35fd4bf0ff041589899d6d4ab167
M-Laetitia <molin.laetitia@gmail.com>
2025-02-14 15:01:50 +0100

    Style: refactor Navbar layout for improved structure and responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,6 +11,7 @@ export default function Navbar() {
         <>
         {/* // Navbar mobile */}
         <nav className=" fixed md:hidden bottom-0 w-full h-[75px] flex items-center justify-between py-5 px-10 bg-darkGrey text-neutralWhite rounded-[20px_20px_0_0]">
+            
             <Link href="/" className="text-2xl hover:text-accentColor">
                 <GoHomeFill />
             </Link>
@@ -30,43 +31,48 @@ export default function Navbar() {
                 </SignedOut>
                 <SignedIn >
                     <UserButton 
-                     appearance={{
+                    appearance={{
                         elements: {
                         //   userButtonAvatarBox: "rounded-full border-2 border-blue-500",
                         },
-                      }}/>
-                   
+                    }}/>
+                
                 </SignedIn>
             </div>
+            
         </nav>
 
         
         {/* // Navbar desktop */}
-        <nav className=" hidden md:flex  fixed top-0 w-full h-[125px] items-center justify-between py-5 px-10 bg-darkGrey text-neutralWhite ">
-            <Link href="/" className="text-2xl hover:text-accentColor">
-                <GoHomeFill />
-            </Link>
-            <Link href="/collections/user" className="text-2xl hover:text-accentColor">
-                <p>My collections </p>
-            </Link>
+        {/* <div className="max-w-[75%] bg-green-400"></div> */}
 
-            <Link href="#" className="text-2xl hover:text-accentColor">
-                My Progress
-            </Link>
-            <div className="flex flex-row-reverse items-center">
-                <SignedOut>
-                    <SignInButton />
-                    <SignUpButton />
-                </SignedOut>
-                <SignedIn >
-                    <UserButton 
-                     appearance={{
-                        elements: {
-                        //   userButtonAvatarBox: "rounded-full border-2 border-blue-500",
-                        },
-                      }}/>
-                   
-                </SignedIn>
+        <nav className=" bg-darkGrey flex content-center justify-center">
+            <div className=" hidden max-w-[75%] md:flex top-0 w-full h-[125px] items-center justify-between py-5 px-10  text-neutralWhite ">
+                <Link href="/" className="text-2xl hover:text-accentColor">
+                    <GoHomeFill />
+                </Link>
+                <Link href="/collections/user" className="text-2xl hover:text-accentColor">
+                    <p>My collections </p>
+                </Link>
+
+                <Link href="#" className="text-2xl hover:text-accentColor">
+                    My Progress
+                </Link>
+                <div className="flex flex-row-reverse items-center">
+                    <SignedOut>
+                        <SignInButton />
+                        <SignUpButton />
+                    </SignedOut>
+                    <SignedIn >
+                        <UserButton 
+                        appearance={{
+                            elements: {
+                            //   userButtonAvatarBox: "rounded-full border-2 border-blue-500",
+                            },
+                        }}/>
+                    
+                    </SignedIn>
+                </div>
             </div>
         </nav>
         </>

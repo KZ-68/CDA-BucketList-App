ba0a180906969e111f8753dab95e7cc6ec143ff3
M-Laetitia <molin.laetitia@gmail.com>
2025-02-14 09:01:22 +0100

    fix: add missing font

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${mainFont} ${secondaryFont} antialiased`}
+
       >
         <header>
           <Navbar />

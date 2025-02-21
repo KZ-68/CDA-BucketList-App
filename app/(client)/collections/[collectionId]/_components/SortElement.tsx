865849a8bd751c6948abfab4795e45ce53235c86
Molina-Q <quentinmolina1@gmail.com>
2025-02-21 11:46:53 +0100

    Style: update SortElement component to use pathname and searchParams for sorting functionality

--- app/(client)/collections/[collectionId]/_components/SortElement.tsx
+++ app/(client)/collections/[collectionId]/_components/SortElement.tsx
@@ -1,20 +1,33 @@
 "use client"
+
+import { usePathname, useSearchParams } from "next/navigation";
+
 interface Props {
-    toggleSort: (sortType: string, currentSearchParams: URLSearchParams) => void;
+    toggleSort: (
+        sortType: string,
+        currentSearchParams: {
+            pathname: string,
+            searchParams: URLSearchParams
+        }
+    ) => void;
 }
 
 export function SortElement({ toggleSort }: Props) {
-    const currentSearchParams = new URLSearchParams(window.location.search);
+    const currentSearchParams = {
+        pathname: usePathname(),
+        searchParams: useSearchParams()
+    }
+
     return (<>
         <p>Sort by : </p>
         <select
             className="bg-[#506382] outline-none p-1"
             onChange={(e) => toggleSort(e.target.value, currentSearchParams)}
+            defaultValue={currentSearchParams.searchParams.get('sortBy') || ""}
         >
-            <option value="">None</option>
             <option value="priority">Priority</option>
             <option value="label">Label</option>
-            <option value="createAt">Date</option>
+            <option value="createdAt">Date</option>
         </select>
     </>
     )

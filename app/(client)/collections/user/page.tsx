2ba844e163c9b82559e4d9da658b3f06833a747a
M-Laetitia <molin.laetitia@gmail.com>
2025-02-21 14:13:03 +0100

    Feat: add filtering options for user collections based on goal status

--- app/(client)/collections/user/page.tsx
+++ app/(client)/collections/user/page.tsx
@@ -7,18 +7,25 @@ import { LuPlus } from "react-icons/lu";
 import Link from 'next/link';
 
 interface Collection {
+    id: string;
     label: string;
     isPrivate : boolean;
     accomplishedGoals: number;
+    goals : Goal[];
     _count?: {
       goals: number;
     };
 }
 
+interface Goal {
+    id: string;
+    isAccomplished: boolean;
+}
+
 const CollectionsPage = () => {
     const [collections, setCollections] = useState<Collection[]>([]);
+    const [filter, setFilter] = useState<string>('All');
     const { userId } = useAuth(); 
-    // const { user } = useUser();
   
     useEffect(() => {
       if (!userId) return;
@@ -36,7 +43,16 @@ const CollectionsPage = () => {
       fetchCollections();
     }, [userId]);
 
-    console.log('collections', collections)
+    // console.log('collections', collections)
+    
+    const filteredCollections = collections.filter((collection) => {
+      const totalGoals = collection.goals.length;
+      if (filter === 'All') return true;
+      if (filter === 'Completed') return collection.accomplishedGoals === totalGoals;
+      if (filter === 'In Progress') return collection.accomplishedGoals > 0 && collection.accomplishedGoals < totalGoals;
+      if (filter === 'Not Started') return collection.accomplishedGoals === 0;
+      return true;
+  });
   
     return (
       <>
@@ -45,11 +61,34 @@ const CollectionsPage = () => {
         <div className='text-lg mb-20'>
           <div className='flex flex-col gap-3 mb-6 '>
             <p>State :</p>
+            
             <div className='flex gap-2 flex-wrap'>
-              <p className='bg-darkGrey border border-solid border-neutralWhite px-5 py-2 rounded-md text-base cursor-pointer hover:border-accentColor hover:text-accentColor'>Completed</p>
-              <p className='bg-darkGrey border border-solid border-neutralWhite px-5 py-2 rounded-md text-base cursor-pointer hover:border-accentColor hover:text-accentColor'>In Progress</p>
-              <p className='bg-darkGrey border border-solid border-neutralWhite px-5 py-2 rounded-md text-base cursor-pointer hover:border-accentColor hover:text-accentColor'>Not Started</p>
-            </div>
+              <p
+                  className={`bg-darkGrey border border-solid border-neutralWhite px-5 py-2 rounded-md text-base cursor-pointer hover:border-accentColor hover:text-accentColor ${filter === 'Completed' ? 'border-accentColor text-accentColor' : ''}`}
+                  onClick={() => setFilter('Completed')}
+              >
+                  Completed
+              </p>
+              <p
+                  className={`bg-darkGrey border border-solid border-neutralWhite px-5 py-2 rounded-md text-base cursor-pointer hover:border-accentColor hover:text-accentColor ${filter === 'In Progress' ? 'border-accentColor text-accentColor' : ''}`}
+                  onClick={() => setFilter('In Progress')}
+              >
+                  In Progress
+              </p>
+              <p
+                  className={`bg-darkGrey border border-solid border-neutralWhite px-5 py-2 rounded-md text-base cursor-pointer hover:border-accentColor hover:text-accentColor ${filter === 'Not Started' ? 'border-accentColor text-accentColor' : ''}`}
+                  onClick={() => setFilter('Not Started')}
+              >
+                  Not Started
+              </p>
+              <p
+                  className={`bg-darkGrey border border-solid border-neutralWhite px-5 py-2 rounded-md text-base cursor-pointer hover:border-accentColor hover:text-accentColor ${filter === 'All' ? 'border-accentColor text-accentColor' : ''}`}
+                  onClick={() => setFilter('All')}
+              >
+                  All
+              </p>
+          </div>
+
           </div>
           <div className='flex flex-col gap-3  '>
             <p>Sort by :</p>
@@ -60,19 +99,22 @@ const CollectionsPage = () => {
             </div>
           </div>
         </div>
+
+
   
         <div className='flex  flex-col gap-12 mb-16'>
           {collections.length === 0 ? (
             <p>Loading...</p>
           ) : (
-            collections.map((collection :Collection, index: number) => (
+            filteredCollections.map((collection :Collection, index: number) => (
               <div key={collection.label || index} className='flex flex-col'>
-                {/* <span> - {collection._count?.goals || 0} {collection._count?.goals === 1 || collection._count?.goals === 0   ? 'goal' : 'goals'}</span> */}
+                
                 <CollectionItem 
                   label = {collection.label}
                   totalGoal = {collection._count?.goals || 0}
                   achievedGoal= {collection.accomplishedGoals}
-                  isPrivate = {collection.isPrivate} />
+                  isPrivate = {collection.isPrivate} 
+                  id = {collection.id}/>
               </div>
             ))
           )}

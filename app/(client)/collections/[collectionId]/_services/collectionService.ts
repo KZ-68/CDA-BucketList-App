7a25d6a663e6e2dc25fdea63d0e6233f2a4aa760
Molina-Q <quentinmolina1@gmail.com>
2025-03-07 11:40:22 +0100

    feat: implement goal creation and category fetching in collection service

--- app/(client)/collections/[collectionId]/_services/collectionService.ts
+++ app/(client)/collections/[collectionId]/_services/collectionService.ts
@@ -4,13 +4,24 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function isOwnerLogged(collecId: string) {
+    "use server"
     const Auth = await auth();
-    const { collection } = await fetchUserCollection(collecId);
+    console.log("collecId :", collecId);
+
+    const collection = await db.collection.findUnique({
+        where: {
+            id: collecId,
+        }
+    });
+    console.log("collection :", collection);
+
+    if (!collection) return false;
 
     const loggedUserId = Auth.userId;
+    console.log("loggedUserId :", loggedUserId);
     if (!loggedUserId) return false;
 
-    return collection.userId === loggedUserId;
+    return collection?.userId === loggedUserId;
 }
 
 export async function fetchUserCollection(
@@ -45,6 +56,46 @@ export async function fetchUserCollection(
     });
 
     return { collection: data.data, goals, totalGoalsCount };
+}
+
+export async function fetchCategories() {
+    const categories = await db.category.findMany();
+
+    return { categories };
+}
+
+export async function createGoal(formData: FormData) {
+    "use server"
+
+    const label = formData.get('label')?.toString() || '';
+    const categoryId = formData.get('categoryId')?.toString() || '';
+    const description = 'None for now';
+    const isAccomplished = false;
+    const priority = 1;
+    const collectionId = formData.get('collectionId')?.toString() || '';
+
+    const goalData = { label, description, isAccomplished, priority, collectionId, categoryId };
+
+
+
+    console.log("goalData :", goalData);
+
+    const baseUrl = process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000'
+        : 'https://cda-bucket-list-app.vercel.app';
+
+    const response = await fetch(`${baseUrl}/api/goals`, {
+        method: 'POST',
+        body: JSON.stringify(goalData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const data = await response.json();
+
+    console.log("data :", data.data);
+
 }
 
 export async function togglePrivacy(privacy: string, collectionId: string) {
@@ -100,14 +151,15 @@ export async function toggleCollection(
 export async function toggleGoal(goalId: string) {
     "use server"
 
-    const isOwner = await isOwnerLogged(goalId);
-    if (!isOwner) return;
-
     const currentGoal = await db.goal.findUnique({
         where: {
             id: goalId
         }
     });
+
+    const isOwner = await isOwnerLogged(currentGoal?.collectionId || '');
+
+    if (!isOwner) return;
 
     if (!currentGoal) return;
 

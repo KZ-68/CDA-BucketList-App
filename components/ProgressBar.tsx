e7b6e5dba81e541c4b5bfbbdb5468e882a394f17
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-03-07 16:27:41 +0100

    style: add props for bg color

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 interface ProgressBarProps {
+  bg_color: string;
   progress: string;
 }
 
-const ProgressBar: React.FC<ProgressBarProps> = ({ progress }) => {
+const ProgressBar: React.FC<ProgressBarProps> = ({ bg_color, progress }) => {
   return (
     <div className="w-[300px] bg-[#18243c] rounded-full h-8 relative border-[2px] border-gray-600">
       <div
-        className="bg-[#30c4e4] h-7 rounded-full transition-all duration-300"
+        className={`${bg_color} h-7 rounded-full transition-all duration-300`}
         style={{ width: `${progress}%` }}
       ></div>
     </div>

3168ac50468c30e03ec7a09b269a62c8d94aef5b
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-03-14 16:45:13 +0100

    refactor: remove unused import

--- app/api/collections/all/route.ts
+++ app/api/collections/all/route.ts
@@ -1,5 +1,5 @@
 import { db } from "@/lib/db";
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { clerkClient } from '@clerk/nextjs/server';
 
 export async function GET() {

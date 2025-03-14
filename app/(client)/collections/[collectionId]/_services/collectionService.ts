11a6eaa817dd02a87defa8247ebc68972218f16b
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-03-14 16:34:43 +0100

    refactor: remove unused import

--- app/(client)/collections/[collectionId]/_services/collectionService.ts
+++ app/(client)/collections/[collectionId]/_services/collectionService.ts
@@ -1,7 +1,6 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { revalidateTag } from "next/cache";
-import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function isOwnerLogged(collecId: string) {

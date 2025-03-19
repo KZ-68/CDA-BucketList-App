b49a2a04ab4cc19c4dda4d8fa854220273a62136
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-03-19 14:23:29 +0100

    test: locahost url

--- components/createCollection.tsx
+++ components/createCollection.tsx
@@ -3,8 +3,6 @@ import { auth } from '@clerk/nextjs/server'
 
 export default async function createCollection(prevState: { success: boolean; message: string; } | Promise<{ success: boolean; message: string; } | null> | null, formData:FormData) {
     const { userId } = await auth()
-    let url = "";
-    const autorization = process.env.VERCEL_TOKEN ? 'Bearer ' + process.env.VERCEL_TOKEN : ""
 
     if (!userId) return { success: false, message: 'User not authentified'};
 
@@ -19,22 +17,14 @@ export default async function createCollection(prevState: { success: boolean; me
         isPrivate : collectionisPrivate,
         userId: userId
     })
-    
-    if(process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
-        url = process.env.NEXT_PUBLIC_URL + `/api/collections`
-    } else {
-        url = "https://" + process.env.VERCEL_URL  + `/api/collections`
-        console.log(url);
-    }
 
-    
-    const response = await fetch(url, {
+    const response = await fetch(process.env.NEXT_PUBLIC_URL + `/api/collections`, {
         method: 'POST',
-        body: JSON.stringify(bodyForm),
         headers: {
             "Content-Type": "application/json",
-            Autorization: autorization
-        }
+        },
+        credentials: "include",
+        body: JSON.stringify(bodyForm),
     })
 
     if(response.ok) {

cbf201a7bf553135b83f5dfe4d7da48d76e14ddd
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-04-22 16:58:26 +0200

    feat: add connection, get collections and goals test

--- __tests__/db.test.ts
+++ __tests__/db.test.ts
@@ -14,3 +14,24 @@ describe('Prisma MongoDB Connection', () => {
   });
 });
 
+describe('Prisma MongoDB GET Collections', () => {
+    it('should connect and fetch collections (even if empty)', async () => {
+        const collections = await db.collection.findMany();
+        expect(Array.isArray(collections)).toBe(true);
+    });
+
+    afterAll(async () => {
+        await db.$disconnect();
+    });
+});
+
+describe('Prisma MongoDB GET Goals', () => {
+    it('should connect and fetch goals (even if empty)', async () => {
+        const goals = await db.goal.findMany();
+        expect(Array.isArray(goals)).toBe(true);
+    });
+
+    afterAll(async () => {
+        await db.$disconnect();
+    });
+});

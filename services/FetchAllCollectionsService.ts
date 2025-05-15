61fcc78afbb7b4e672de7fec5a7ca72318fb9958
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-05-15 23:50:54 +0200

    feat: change fetching method with useSWR hook for FetchUserFavoriteCollectionsService service

--- services/FetchAllCollectionsService.ts
+++ services/FetchAllCollectionsService.ts
@@ -5,7 +5,7 @@ const fetcher = (url:string) => fetch(url).then((res) => res.json());
 const FetchAllCollectionsService = ()  => {
   const { data: dataCollection } = useSWR('/api/collections/all', fetcher);
 
-    return dataCollection;
+  return dataCollection;
 }
 
 export default FetchAllCollectionsService;

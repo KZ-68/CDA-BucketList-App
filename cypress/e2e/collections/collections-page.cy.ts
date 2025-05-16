49681c088540f7f0375d46aecdb2d2e74e7593af
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-05-16 21:06:30 +0200

    test: update env variables

--- cypress/e2e/collections/collections-page.cy.ts
+++ cypress/e2e/collections/collections-page.cy.ts
@@ -3,9 +3,9 @@ it('Visit My Collections', () => {
   cy.get("[component='SignInButton']").click({ multiple: true, force: true });
 
   cy.origin('https://glorious-moray-43.accounts.dev/sign-in', () => {
-    cy.get("[id='identifier-field']").type(Cypress.env("clerk-email"));
+    cy.get("[id='identifier-field']").type(Cypress.env("CLERK_EMAIL"));
     cy.get("button").click({multiple:true, force:true});
-    cy.get("[id='password-field']").type(Cypress.env("clerk-password"));
+    cy.get("[id='password-field']").type(Cypress.env("CLERK_PASSWORD"));
     cy.get("[class='cl-formButtonPrimary cl-button 🔒️ cl-internal-9c635s']").click({multiple:true, force:true});
   })
 })
